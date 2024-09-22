7a21b203ebda255a94ebb6649efc3ca51cec0e2e
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2024-09-22 22:48:49 +0530

    Remove_characters

---
+++ STRINGS/17_remove_characters.c
@@ -0,0 +1,40 @@
+//WRITE A PROGRAM IN C TO REMOVE CHARACTERS FROM A STRING EXCEPT ALPHABETS
+
+#include<stdio.h>
+#include<string.h>
+
+// FUNCTION DECLERATION
+
+void remove(char []);
+
+//  MAIN FUNCTION
+
+void main()
+{
+    char string[100];
+
+    printf("Input the string : ");
+    fgets(string, sizeof(string), stdin);
+
+    remove(string);
+}
+
+// REMOVE FUNCTION DEFINITION
+
+void remove(char string[])
+{
+    int i,j;
+    for(i = 0; string[i] != '\0'; i++)
+    {
+        if((string[i] < 65 && string[i] > 90) || (string[i] < 96 && string[i] > 122))
+        {
+            for(j = i; string[j] != '\0'; j++)
+            {
+                string[j] = string[j + 1];
+            }
+            string[j] = '\0';
+        }
+    }   
+
+    printf("After removing characters except alphabets : %s", string);
+}
