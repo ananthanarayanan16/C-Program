802321c0699bcc4d99907a97032c00f84f0a1038
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2024-11-25 23:30:18 +0530

    Sting

--- CONDITIONS/condition-14.c
+++ CONDITIONS/condition-14.c
@@ -6,7 +6,7 @@ void main()
 	int side1,side2,side3;
 	printf("Test data  :  ");
 	scanf("%d%d%d",&side1,&side2,&side3);
-	if(side1==side2&&side1==side3&&side2==side3)
+	if(side1==side2&&side1==side3) //&&side2==side3)
 	{
 		printf("This is Equilateral Triangle");
 	}
