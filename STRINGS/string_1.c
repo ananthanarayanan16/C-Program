6c7955916d8e59ac7cae6b68ab7561d1a8af578a
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2024-12-15 21:00:07 +0530

    String

---
+++ STRINGS/string_1.c
@@ -0,0 +1,12 @@
+//  WRITE A PROGRAM C IN STRING
+
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char ch ;
+    printf("Input a cahracters : ");
+    scanf("%c", &ch);
+    printf("The given string : %c", ch);
+}
