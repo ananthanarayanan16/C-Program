26a8e61f218a037c7da06a90cd655d095d18c8fa
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-01-01 23:04:02 +0530

    String

---
+++ STRINGS/string.c
@@ -0,0 +1,13 @@
+// WRITE A C PROGRAM TO MULTIPLY TO POSITIVE NUMBER AS STRING. RETURN A STRING REPRESENTATION OF THE PRODUCT.
+
+#include<stdio.h>
+#include<string.h>
+
+int number(char []);
+char string(int);
+
+void main()
+{
+    int num_1, num_2, result;
+    char str_1[100], str_2[100], product[100];
+}
