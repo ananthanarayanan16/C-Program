8b012278e573c112f3d140d223d0783aa5999986
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-01-06 23:23:48 +0530

    String

---
+++ STRINGS/string.c
@@ -0,0 +1,14 @@
+// wwrite a program in c 
+
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char ch;
+
+    printf("Enter a character : ");
+    scanf("%c", &ch);
+
+    printf("The given charactes : %c", ch);
+}
