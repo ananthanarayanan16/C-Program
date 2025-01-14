b52361f44a79f8966db61752694b483b5fb5a358
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-01-14 11:00:05 +0530

    String

---
+++ STRINGS/40_string.c
@@ -0,0 +1,14 @@
+// WRITE A C PROGRAM IN C TO FIND THE STRING
+
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char ch;
+
+    printf("Enter the characters : ");
+    scanf("%c", &ch);
+
+    printf("the given string : %c", ch);
+}
