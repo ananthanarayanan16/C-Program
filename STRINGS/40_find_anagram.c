7049b011cd589dab71597eb53ae61407193721c6
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-01-21 21:03:03 +0530

    Find_The_Given_Strings_Is_Anagram_Or_Not

--- STRINGS/40_find_anagram.c
+++ STRINGS/40_find_anagram.c
@@ -3,8 +3,12 @@
 #include<stdio.h>
 #include<string.h>
 
+// FUNCTION DECLERATION
+
 void anagram(char [], char []);
 int length(char []);
+
+// MAIN FUNCTION
 
 void main()
 {
@@ -19,31 +23,60 @@ void main()
     anagram(str_1, str_2);
 }
 
+// ANAGRAM FUNCTION DEFINITATION
+
 void anagram(char str_1[], char str_2[])
 {
-    int i, count = 0, len;
+    int n, flag = 0, len_1, len_2, count_1 = 0, count_2 = 0;
 
-    for(i = 0; str_1[i] != '\0' && str_1[i] != '\n'; i++)
+    len_1 = length(str_1);
+    len_2 = length(str_2);
+
+    if(len_1 == len_2)
     {
-        for(int j = 0; str_2[j] != '\0' && str_2[j] != '\n'; j++)
+        // FIND ANAGRAM TO COMPARE STRING CHARACTES
+
+        for(int i = 0; i < len_1; i++)
         {
-            if(str_1[i] == str_2[j])
+            for(int j = 0; str_1[j] != '\0' && str_1[j] != '\n'; j++)
             {
-                count++;
+                if(str_2[i] == str_1[j])
+                {
+                    count_1++;
+                    n = j;
+                }
+            }
+            for(int k = 0; str_2[k] != '\0' && str_2[k] != '\n'; k++)
+            {
+                if(str_1[n] == str_2[k])
+                {
+                    count_2++;
+                }
+            }
+            if(count_1 == count_2)
+            {
+                continue;
+            }
+            else
+            {
+                printf("The given strings are Not Anagram.\n");
+                flag++;
                 break;
             }
         }
-    }
-    len = length(str_1);
-    if(count == len)
-    {
-        printf("%s and %s are Anagram.\n",str_1, str_2);
+       
     }
     else
     {
-        printf("%s and %s are not Anagram.\n", str_1, str_2);
+        printf("The given two string are Not Anagram.\n");
+    }
+    if(flag == 0)
+    {
+        printf("The given two strings are Anagram.\n");
     }
 }
+
+// FIND LENGTH OF THE STRINGS
 
 int length(char str[])
 {
@@ -56,3 +89,20 @@ int length(char str[])
 
     return count;
 }
+
+
+/*
+
+output 1:
+
+Enter the first string : spear
+Enter the second string : pears
+The given two strings are Anagram.
+
+output 2:
+
+Enter the first string : pss
+Enter the second string : spp
+The given strings are Not Anagram.
+
+*/
