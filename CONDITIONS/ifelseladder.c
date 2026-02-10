92c6e0a3f539d850e6c3db29d87e382ef9225396
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2026-02-10 23:29:32 +0530

    Updates

--- CONDITIONS/ifelseladder.c
+++ CONDITIONS/ifelseladder.c
@@ -1,30 +1,49 @@
-//IFeELSE LADDER
+//IFELSE LADDER
 
-#include<stdio.h>
-void main()
-{
-	int x,y;
-	printf("Enter x and y value  :  ");
-	scanf("%d%d",&x,&y);
-	if(x<y)
-	{
-		printf("X is less than Y");
-	}
-	else if(x==y)
-	{
-		printf("X is equal to Y");
-	}
-	else if(x!=y)
-	{
-		printf("X is not equal to Y");
-	}
-	else
-	{
-		printf("X is greater than Y");
-	}
-	printf("\nProgram is finished.");
-}
+// #include<stdio.h>
+// void main()
+// {
+// 	int x,y;
+// 	printf("Enter x and y value  :  ");
+// 	scanf("%d%d",&x,&y);
+// 	if(x<y)
+// 	{
+// 		printf("X is less than Y");
+// 	}
+// 	else if(x==y)
+// 	{
+// 		printf("X is equal to Y");
+// 	}
+// 	else if(x!=y)
+// 	{
+// 		printf("X is not equal to Y");
+// 	}
+// 	else
+// 	{
+// 		printf("X is greater than Y");
+// 	}
+// 	printf("\nProgram is finished.");
+// }
 /* OUTPUT:
 Enter x and y value  :  12 12
 X is equal to Y
 Program is finished.  */		
+
+
+
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("Enter two numbers : ");
+	scanf("%d%d",&x,&y);
+	if(x>y)
+	{
+		printf("X is greater than Y.\n");
+	}
+	else
+	{
+		printf("%d is greater than %d.\n",y,x);
+	}
+	return 0;
+}
